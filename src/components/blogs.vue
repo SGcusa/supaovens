16d1766b9521fda1d64eb19afe5b8d7be06b77da
Asisipho10 <asisipho@wbwr.io>
2025-02-22 20:47:17 +0200

    wip

--- src/components/blogs.vue
+++ src/components/blogs.vue
@@ -1,59 +1,81 @@
+
+<script>
+export default {
+  name: "Badge",
+};
+</script>
+
 <template>
-  <div class="blog-section bg-black">
-    <div class="blogs-container w-full mx-auto my-0">
-      <div class="header-section flex flex-row items-center sm:justify-center py-10">
-        <h1 class="text-white px-[10px] sm:px-[10px] text-[24px] sm:text-[40px]">NEWS & UPDATES:</h1>  
-      </div>
-      <div class="blogs-block pb-20 max-w-[1280px] overflow-x-scroll mx-auto px-[10px] sm:px-10">
-        <div class="flex flex-row items-center gap-6 lg:gap-10 mx-auto min-w-[1000px]">
-          <div class="w-1/3 flex flex-col justify-between">
-            <div class="flex flex-col justify-between text-white h-[200px]">
-              <div class="blog-header text-[16px] sm:text-[18px]">Summer Relaxation Special</div>
-              <div class="blog-text py-6 text-[12px]">Escape the summer heat and indulge in our Summer Relaxation Special! From August 15th to 30th, enjoy 20% off all massage treatments. Choose from our range of therapeutic massages, including Swedish, Deep Tissue, and Aromatherapy, and let our skilled therapists melt away your tension and stress.</div>
-              <div class="blog-post-date flex flex-row justify-between">
-                <div class="blog-date text-[14px]">August 15-30:</div>
-                <div class="blog-navigate text-[14px] underline">READ MORE</div>
-              </div>
-            </div>
-            <div class="blog-img h-[440px] pt-3 overflow-hidden">
-              <a href="">
-                <img class="w-full h-full object-cover justify-center" src="https://cdn.shopify.com/s/files/1/0900/4875/8049/files/unsplash_87InWldRhgs.png?v=1736324962" alt="">
-              </a>
-            </div>
-          </div>
-          <div class="w-1/3 flex flex-col justify-between">
-            <div class="flex flex-col justify-between text-white h-[200px]">
-              <div class="blog-header text-[16px] sm:text-[18px]">Skincare 101 Seminar</div>
-              <div class="blog-text py-6 text-[12px]">Curious about how to achieve glowing, healthy skin? Our Skincare 101 Seminar on August 28th from 2:00 PM to 4:00 PM is perfect for you. Our expert estheticians will share tips on creating a personalized skincare routine, the importance of SPF, and how to choose the right products for your skin type.</div>
-              <div class="blog-post-date flex flex-row justify-between">
-                <div class="blog-date text-[14px]">August 28:</div>
-                <div class="blog-navigate text-[14px] underline">READ MORE</div>
-              </div>
-            </div>
-            <div class="blog-img h-[440px] pt-3 overflow-hidden">
-              <video autoplay loop playsinline muted>
-                <source class="w-full h-full object-cover justify-center" src="https://cdn.shopify.com/videos/c/o/v/2ee482a37bd3418885f936bb495600cc.mp4" type="video/mp4">
-              </video>
-            </div>
-          </div>
-          <div class="w-1/3 flex flex-col justify-between">
-            <div class="flex flex-col justify-between text-white h-[200px]">
-              <div class="blog-header text-[16px] sm:text-[18px]">Yoga and Wellness Workshop</div>
-              <div class="blog-text py-6 text-[12px]">Join us for a rejuvenating Yoga and Wellness Workshop on August 20th from 10:00 AM to 1:00 PM. This event will be held in our peaceful garden and includes a gentle yoga session, guided meditation, and a wellness talk on the benefits of incorporating mindfulness practices into your daily routine.</div>
-              <div class="blog-post-date flex flex-row justify-between">
-                <div class="blog-date text-[14px]">August 20:</div>
-                <div class="blog-navigate text-[14px] underline">READ MORE</div>
-              </div>
-            </div>
-            <div class="blog-img h-[440px] pt-3 overflow-hidden">
-              <a href="">
-                <img class="w-full h-full object-cover justify-center" src="https://cdn.shopify.com/s/files/1/0900/4875/8049/files/unsplash__ZZNuZxgv9o.png?v=1736324962" alt="">
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>  
-  </div>
+  <div class="badges" style="--width: 160px; --height: 160px; --quantity: 22;">
+    <div class="badge-list">
+      <div style="--position: 1;" class="badge-icon"><img src="https://i.ibb.co/7ynjv4V/SCRUM.png" alt="icon" class="badges" loading="lazy"></div>
+      <div style="--position: 2;" class="badge-icon"><img src="https://i.ibb.co/QcC8Pr5/Figma-1.png" alt="icon" class="badges" loading="lazy"></div>
+      <div style="--position: 3;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/3b/8c/d3/ME18SVEM_t.png" alt="icon" class="badges" loading="lazy"></div>
+      <div style="--position: 4;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/9f/23/34/ME18SX5N_t.png" alt="icon" class="badges" loading="lazy"></div>
+      <div style="--position: 5;" class="badge-icon"><img src="https://i.ibb.co/3RNxFqF/Screenshot-2024-06-25-114005-removebg-preview.png" alt="icon" class="badges" loading="lazy"></div>
+      <div style="--position: 6;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/2a/2f/db/ME18SVEW_t.png" alt="icon" class="badges" loading="lazy"></div>
+      <div style="--position: 7;" class="badge-icon"><img src="https://i.ibb.co/pXGW8Jp/icons8-html-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 8;" class="badge-icon"><img src="https://i.ibb.co/YDW5rr9/icons8-css-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 9;" class="badge-icon"><img src="https://i.ibb.co/VwtVMMN/icons8-bootstrap-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 10;" class="badge-icon"><img src="https://i.ibb.co/C8z52hz/icons8-js-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 11;" class="badge-icon"><img src="https://i.ibb.co/wpW236s/icons8-mysql-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 12;" class="badge-icon"><img src="https://i.ibb.co/MGFkCRy/icons8-node-js-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 13;" class="badge-icon"><img src="https://i.ibb.co/G3DNphW/icons8-vue-js-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 14;" class="badge-icon"><img src="https://i.ibb.co/ThgfnV7/icons8-git-48.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 15;" class="badge-icon"><img src="https://i.ibb.co/brX374p/python-1.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 16;" class="badge-icon"><img src="https://i.ibb.co/n0CmCpc/c-1.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 17;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/37/c8/a4/ME18MRYK_t.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 18;" class="badge-icon"><img src="https://i.ibb.co/Wp4JQ0p/linux.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 19;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/88/d0/c9/ME18SVFE_t.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 20;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/cf/c2/e4/ME18SVFF_t.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 21;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/8f/92/f8/ME18SVFD_t.png" alt="icon" loading="lazy"></div>
+    <div style="--position: 22;" class="badge-icon"><img src="https://cdn-thumbs.imagevenue.com/c6/0e/15/ME18SVFC_t.png" alt="icon" loading="lazy"></div>
+    </div>
+  </div>  
 </template>
-  
+
+<style scoped>
+  .badges {
+    width: 100%;
+    height: var(--height);
+    overflow: hidden;
+    mask-image: 
+      linear-gradient(
+        to right, 
+        transparent, 
+        #000 10% 90%, 
+        transparent
+      )
+    ;
+  }
+  .badges .badge-list {
+    display: flex;
+    width: 100%;
+    min-width: calc(var(--width) * var(--quantity));
+    position: relative;
+  }
+  .badge-icon {
+    width: var(--width);
+    height: var(--height);
+    position: absolute;
+    left: 100%;
+    animation: autoRun 40s linear infinite;
+    animation-delay: calc( (40s / var(--quantity)) * (var(--position) - 1) - 8s);
+  }
+  .badges .badge-list .badge-icon img {
+    width: 80%;
+    height: 80%;
+    filter: grayscale(100%);
+  }
+  @keyframes autoRun {
+    from {
+        transform: translateX(calc(-100% - var(--width) * var(--quantity)));
+    }
+    to {
+        transform: translateX(100%);
+    }
+  }
+  .badges:hover .badge-icon {
+    animation-play-state: paused!important;
+  }
+</style>
